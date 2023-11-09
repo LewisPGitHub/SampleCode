4569de41f33e94e050f8c044b6b699cd59839681
lewisGithub <77052920+LewisPGitHub@users.noreply.github.com>
2023-11-09 12:40:55 +0000

    First upload of Simple madlibs

---
+++ madlib.py
@@ -0,0 +1,8 @@
+#mad libs
+verb = input ("Give me a verb ")
+adjective = input ("Give me an adjective ")
+noun = input ("Give me a noun ")
+adverb = input ("Give me an adverb ")
+
+# creates sentence from user inputs
+print (f"The {noun} extremely {adjective} whilst  {verb} until {adverb} and so they became rich.")
